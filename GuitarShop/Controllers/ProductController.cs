e45aa58d4f672e75a42def26d577f14c52f50057
gnarlsley <thompson.garrett7413@gmail.com>
2023-11-12 17:14:06 -0600

    Added a new category of Strings outside of the database. Displayed an extra button for the new category

--- GuitarShop/Controllers/ProductController.cs
+++ GuitarShop/Controllers/ProductController.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿/*Name Garrett Thompson
+ * Date 11/12/23
+ */
+
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using GuitarShop.Models;
